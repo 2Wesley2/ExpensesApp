eb825c940b05cb8609547951796be1b05f397d37
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-01-27 12:55:48 -0300

    exibindo Switch class na tela apenas quando o dispositivo estiver em modo paisagem

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -15,18 +15,15 @@ class TransactionList extends StatelessWidget {
         ? LayoutBuilder(builder: (ctx, constraints) {
             return Column(
               children: <Widget>[
-                SizedBox(
-                  height: constraints.maxHeight * 0.05,
+                const SizedBox(
+                  height: 20,
                 ),
-                SizedBox(
-                  height: constraints.maxHeight * 0.3,
-                  child: Text(
-                    'Nenhuma Transação Cadastrada!',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
+                Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                SizedBox(
-                  height: constraints.maxHeight * 0.05,
+                const SizedBox(
+                  height: 20,
                 ),
                 SizedBox(
                   height: constraints.maxHeight * 0.6,
